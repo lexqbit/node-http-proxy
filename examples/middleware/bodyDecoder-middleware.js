7f3550b7266e377ef4edae9220309faa38047b42
trestletech <jeff.allen@trestletechnology.net>
2015-06-16 17:33:38 -0500

    Merged from master. Forfeits most of our changes.

--- examples/middleware/bodyDecoder-middleware.js
+++ examples/middleware/bodyDecoder-middleware.js
@@ -1,87 +1,93 @@
+/*
+  bodyDecoder-middleware.js: Basic example of `connect.bodyParser()` middleware in node-http-proxy
 
-var Store = require('../helpers/store')
-  , http = require('http')
+  Copyright (c) Nodejitsu 2013
 
-http.createServer(new Store().handler()).listen(7531, function () {
-//try these commands:
-// get index:
-// curl localhost:7531
-// []
-//
-// get a doc:
-// curl localhost:7531/foo
-// {"error":"not_found"}
-//
-// post an doc:
-// curl -X POST localhost:7531/foo -d '{"content": "hello", "type": "greeting"}'
-// {"ok":true}
-//
-// get index (now, not empty)
-// curl localhost:7531
-// ["/foo"]
-//
-// get doc 
-// curl localhost:7531/foo
-// {"content": "hello", "type": "greeting"}
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
 
-//
-// now, suppose we wanted to direct all objects where type == "greeting" to a different store 
-// than where type == "insult"
-//
-// we can use connect connect-bodyDecoder and some custom logic to send insults to another Store.
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
 
-//insult server:
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-  http.createServer(new Store().handler()).listen(2600, function () {
+*/
 
-  //greetings -> 7531, insults-> 2600 
+var http = require('http'),
+    connect = require('connect'),
+    request = require('request'),
+    colors = require('colors'),
+    util = require('util'),
+    bodyParser = require('body-parser'),
+    httpProxy = require('../../lib/http-proxy'),
+    proxy = httpProxy.createProxyServer({});
 
-  // now, start a proxy server.
 
-    var bodyParser = require('connect/lib/middleware/bodyParser')
-    //don't worry about incoming contont type
-    //bodyParser.parse[''] = JSON.parse
-
-    require('../../lib/node-http-proxy').createServer(
-      //refactor the body parser and re-streamer into a separate package
-      bodyParser(),
-      //body parser absorbs the data and end events before passing control to the next
-      // middleware. if we want to proxy it, we'll need to re-emit these events after 
-      //passing control to the middleware.
-      require('connect-restreamer')(),
-      function (req, res, proxy) {
-        //if your posting an obect which contains type: "insult"
-        //it will get redirected to port 2600.
-        //normal get requests will go to 7531 nad will not return insults.
-        var port = (req.body && req.body.type === 'insult' ? 2600 : 7531)
-        proxy.proxyRequest(req, res, {host: 'localhost', port: port})
+//restreame
+var restreamer = function (){
+  return function (req, res, next) { //restreame
+    req.removeAllListeners('data')
+    req.removeAllListeners('end')
+    next()
+    process.nextTick(function () {
+      if(req.body) {
+        req.emit('data', JSON.stringify(req.body))
       }
-    ).listen(1337, function () {
-      var request = require('request')
-      //bodyParser needs content-type set to application/json
-      //if we use request, it will set automatically if we use the 'json:' field.
-      function post (greeting, type) {
-        request.post({
-          url: 'http://localhost:1337/' + greeting,
-          json: {content: greeting, type: type || "greeting"}
-        })
-      }
-      post("hello")
-      post("g'day")
-      post("kiora")
-      post("houdy")
-      post("java", "insult")
-
-      //now, the insult should have been proxied to 2600
-      
-      //curl localhost:2600
-      //["/java"]
-
-      //but the greetings will be sent to 7531
-
-      //curl localhost:7531
-      //["/hello","/g%27day","/kiora","/houdy"]
-
+      req.emit('end')
     })
+  }
+}
+
+
+//
+//  Basic Http Proxy Server
+//
+var app = connect()
+  .use(bodyParser.json())//json
+  .use(restreamer())//restreame
+  .use(function(req, res){
+    // modify body here,
+    // eg: req.body = {a: 1}.
+    console.log('proxy body:',req.body)
+    proxy.web(req, res, {
+      target: 'http://127.0.0.1:9013'
+    })
+  });
+
+http.createServer(app).listen(8013, function(){
+  console.log('proxy linsten 8013');
+});
+
+
+
+//
+//  Target Http Server
+//
+var app1 = connect()
+  .use(bodyParser.json())
+  .use(function(req, res){
+    console.log('app1:',req.body)
+    res.end('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+  });
+http.createServer(app1).listen(9013, function(){
+  //request to 8013 to proxy
+  request.post({//
+    url: 'http://127.0.0.1:8013',
+    json: {content: 123, type: "greeting"}
+  },function(err, res,data){
+    console.log('return:' ,err, data)
   })
-})
+});
+
+
